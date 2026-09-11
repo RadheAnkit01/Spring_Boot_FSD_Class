08a1fef442ad6054ad5f609ac1d1d4223c7be2fb
Ankit Raj <radheankit110031@gmail.com>
2026-09-11 12:48:38 +0530

    learned how to avoid double posting problem using cookie

--- servlet_class/src/main/java/org/example/server_side_calling/Add.java
+++ servlet_class/src/main/java/org/example/server_side_calling/Add.java
@@ -3,9 +3,7 @@ package org.example.server_side_calling;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.*;
 
 import java.io.IOException;
 
@@ -14,8 +12,31 @@ public class Add extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("Hello from Add class");
-        RequestDispatcher rd = req.getRequestDispatcher("result");
 
-        rd.forward(req,resp);
+
+//        Double POsting Problem
+//        RequestDispatcher rd = req.getRequestDispatcher("result");
+//        rd.forward(req,resp);
+
+//      ----------------------  Solving Double Posting Problem ------------------
+//      -------------  Using Http Session management -----
+        Integer num1 = Integer.parseInt(req.getParameter("num1"));
+        Integer num2 = Integer.parseInt(req.getParameter("num2"));
+
+//      ----------------  Using Cookies --------------
+        Cookie cNum1 = new Cookie("cNum1", req.getParameter("num1"));
+        Cookie cNum2 = new Cookie("cNum2", req.getParameter("num2"));
+
+
+
+        HttpSession session = req.getSession();
+//        session.setAttribute("num1",num1);
+//        session.setAttribute("num2",num2);
+
+        resp.addCookie(cNum1);
+        resp.addCookie(cNum2);
+
+
+        resp.sendRedirect("result");
     }
 }
--- servlet_class/src/main/java/org/example/server_side_calling/Result.java
+++ servlet_class/src/main/java/org/example/server_side_calling/Result.java
@@ -2,9 +2,7 @@ package org.example.server_side_calling;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.*;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -15,15 +13,35 @@ public class Result extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("Hello from Result class");
         resp.setContentType("text/html");
-        Integer num1 = Integer.parseInt(req.getParameter("num1"));
-        Integer num2 = Integer.parseInt(req.getParameter("num2"));
+
+//        ---------------- Using re
+//        Integer num1 = Integer.parseInt(req.getParameter("num1"));
+//        Integer num2 = Integer.parseInt(req.getParameter("num2"));
+
+//        ------------------ Using Session ------------
+//        HttpSession session = req.getSession();
+//        int num1 = (int)session.getAttribute("num1");
+//        int num2 = (int)session.getAttribute("num2");
+
+
+//       --------------------  Using Cookies -----------------
+        Cookie[] cookies = req.getCookies();
+
+        int num1 = 0;
+        int num2 = 0;
+        for(Cookie cookie : cookies){
+            if(cookie.getName().equals("cNum1") ){
+                num1 = Integer.parseInt(cookie.getValue());
+            }
+            if(cookie.getName().equals("cNum2") ) {
+                num2 = Integer.parseInt(cookie.getValue());
+            }
+        }
+
 
         PrintWriter printWriter = resp.getWriter();
-
         req.getRequestDispatcher("header.html").include(req,resp);
-
         printWriter.println("<h1> Sum of %s and %s is : %s</h1>".formatted(num1,num2,num1+num2));
-
         req.getRequestDispatcher("footer.html").include(req,resp);
     }
 }
